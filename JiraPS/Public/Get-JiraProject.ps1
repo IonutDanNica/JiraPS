16abaa03ee52f55e3007d8e2bfec5157bd31da56
Ionut-Dan Nica <14056001+IonutDanNica@users.noreply.github.com>
2024-01-16 19:48:09 +0200

    Update Get-JiraProject.ps1
    Fixed an issue where the blank credentials was making an unauthenticated Invoke-jira method request  that Jira 9.0 does not like

--- JiraPS/Public/Get-JiraProject.ps1
+++ JiraPS/Public/Get-JiraProject.ps1
@@ -1,3 +1,4 @@
+
 function Get-JiraProject {
     # .ExternalHelp ..\JiraPS-help.xml
     [CmdletBinding( DefaultParameterSetName = '_All' )]
@@ -16,8 +17,26 @@ function Get-JiraProject {
         Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
 
         $server = Get-JiraConfigServer -ErrorAction Stop
-
         $resourceURi = "$server/rest/api/2/project{0}?expand=description,lead,issueTypes,url,projectKeys"
+        
+        if ($Credential -and ($Credential -ne [System.Management.Automation.PSCredential]::Empty)) {
+            Write-Verbose "[$($MyInvocation.MyCommand.Name)] Using credential authentication..."
+            $parameter = @{
+                Method     = "GET"
+                Credential = $Credential
+            }    
+        }
+
+        $Headers = (Get-JiraSession).WebSession.headers
+        if (-not ($Headers)) {
+            Write-Error "Unable to obtain authentication headers of the established Jira Session, function exit"
+            return
+        }
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Using bearer authentication..."
+        $parameter = @{
+            Method     = "GET"
+            Headers    = $Headers
+        }
     }
 
     process {
@@ -26,29 +45,24 @@ function Get-JiraProject {
 
         switch ($PSCmdlet.ParameterSetName) {
             '_All' {
-                $parameter = @{
-                    URI        = $resourceURi -f ""
-                    Method     = "GET"
-                    Credential = $Credential
-                }
+                $value = $($resourceURi -f "")
+                $parameter["URI"] = $value
+
                 Write-Debug "[$($MyInvocation.MyCommand.Name)] Invoking JiraMethod with `$parameter"
                 $result = Invoke-JiraMethod @parameter
-
+        
                 Write-Output (ConvertTo-JiraProject -InputObject $result)
             }
             '_Search' {
                 foreach ($_project in $Project) {
                     Write-Verbose "[$($MyInvocation.MyCommand.Name)] Processing [$_project]"
                     Write-Debug "[$($MyInvocation.MyCommand.Name)] Processing `$_project [$_project]"
+                    $value = $resourceURi -f "/$($_project)"
+                    $parameter["URI"] = $value
 
-                    $parameter = @{
-                        URI        = $resourceURi -f "/$($_project)"
-                        Method     = "GET"
-                        Credential = $Credential
-                    }
                     Write-Debug "[$($MyInvocation.MyCommand.Name)] Invoking JiraMethod with `$parameter"
                     $result = Invoke-JiraMethod @parameter
-
+            
                     Write-Output (ConvertTo-JiraProject -InputObject $result)
                 }
             }
